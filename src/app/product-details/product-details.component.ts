e659beac30f5cd43a26c71112c5a211b8044656a
Domingo <30532920+ctagayun@users.noreply.github.com>
2024-11-03 08:08:10 -0600

    save

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { IProduct } from '../catalog/product.model';
 
 @Component({
@@ -11,13 +11,18 @@ export class ProductDetailsComponent {
   //The exclamation point tells Angular to ignore that it is not initialized
   @Input() product!: IProduct;
 
+  //create an event emitter called "buy"
+  @Output() buy = new EventEmitter()
+
   getImageUrl(product: IProduct){
     if(!product) return '';
     return '/assets/images/robot-parts/' + product.imageName;
   }
 
-  addToCart(product: IProduct)
+  buyButtonClick(product: IProduct)
   {
-
+    //when button is clicked raise  @Output() event emitter we created called "buy"
+    //This event is being listened to in CatalogComponent.ts
+    this.buy.emit();
   }
 }
